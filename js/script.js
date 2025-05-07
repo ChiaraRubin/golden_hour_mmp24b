d692d6460d5665938f9de1e2728ce1362dcd2985
Chiara-Vanessa <chiara.rubin@stud.fhgr.ch>
2025-05-07 17:48:07 +0200

    Suchbegriff, der in das Suchfeld gegeben wird, wird in einer Variabel gespeichert +  Button wird in eine Variabel gespeichert

--- js/script.js
+++ js/script.js
@@ -1,69 +1,32 @@
-const sonnenaufgang = await loadSonnenaufgang();
-const sonnenuntergang = await loadSonnenuntergang();
-const tageslaenge = await taegesLaenge();
-const jetzt = new Date();
-const stunden = jetzt.getHours();
-const minuten = jetzt.getMinutes();
-const sekunden = jetzt.getSeconds();
+//Suchbegriff, der in das Suchfeld gegeben wird, wird in einer Variabel gespeichert
+let suchbegriff = "";
+let urlOrtschaft = "";
 
-console.log(`Aktuelle Uhrzeit: ${stunden}:${minuten}:${sekunden}`);
-
-// Sonnenaufgang laden
-async function loadSonnenaufgang() {
-  const urlSunriseSunset =
-    "https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400";
-  try {
-    const response = await fetch(url);
-    const answer = await response.json();
-    return answer.results;
-  } catch (error) {
-    console.error(error);
-    return false;
-  }
+function speichern() {
+  suchbegriff = document.querySelector("#suchFeld").value;
+  urlOrtschaft = `https://nominatim.openstreetmap.org/search?city=${suchbegriff}&format=json`;
 }
 
-// Sonnenuntergang laden
-async function loadSonnenuntergang() {
-  const urlSunriseSunset =
-    "https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400";
-  try {
-    const response = await fetch(url);
-    const answer = await response.json();
-    return answer.results;
-  } catch (error) {
-    console.error(error);
-    return false;
-  }
-}
-
-// Tageslänge laden
-async function taegesLaenge() {
-  const urlSunriseSunset =
-    "https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400";
-  try {
-    const response = await fetch(url);
-    const answer = await response.json();
-    return answer.results;
-  } catch (error) {
-    console.error(error);
-    return false;
-  }
-}
-
-// unsere pokemon ins HTML/DOM einfüllen
-const cards_container = document.querySelector("#cards");
-all_pokemon_with_details.forEach((pokemon) => {
-  const card = `<div class="card">
-                <h2 class="pokemon_name">${pokemon.name}</h2>
-                <img class="pokemon_image" src="${pokemon.image}" alt="">
-                <p class="pokemon_hp">Lebenskraft: <span class="value_hp">${pokemon.hp}</span></p>
-            </div>`;
-  cards_container.innerHTML += card;
+// Button wird in eine Variabel gespeichert + mit dem Klick auf "Suchen" werden die Daten gefeched
+const buttonSuche = document.querySelector("#suchButton");
+buttonSuche.addEventListener("click", function ladeDatenOrt() {
+  speichern();
+  alert("Test");
 });
 
+//Alert wird nicht angezeigt
 // Vorlage Pokemon
 const all_pokemon = await loadPokemon();
 let all_pokemon_with_details = [];
+
+try {
+  const response = await fetch(url);
+  const answer = await response.json();
+  return answer.results;
+} catch (error) {
+  console.error(error);
+  return false;
+}
 
 // pokemon loader
 async function loadPokemon() {
