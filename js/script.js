f78e24a7966c4b78446bbe7da09c99a26c923465
Chiara-Vanessa <chiara.rubin@stud.fhgr.ch>
2025-05-08 16:35:30 +0200

    Klassen für das body Element hinzugefügt

--- js/script.js
+++ js/script.js
@@ -5,6 +5,10 @@ let sonnenAufgang = "";
 let sonnenUntergang = "";
 let aktuelleTageszeit = "";
 let urlSonnenstand = "";
+let backgroundColor = document.querySelector("body");
+
+// Uhrzeit Hintergrundfarbenwechsel
+// Wenn aktuelle Zeit, let jetzt = new Date() kleiner ist als sonnenUntergang, gib mir die Hintergrundfarbe #edc9ad aus. Andernfalls gib die Hintergrundfarbe #2b2b2b aus.
 
 async function sonnenZeiten(lat, lon) {
   urlSonnenstand = `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lon}&tzid=Europe/Berlin`;
@@ -86,63 +90,3 @@ async function loadSonne(url) {
     return false;
   }
 }
-
-//----------------------------------------------------------------------------
-// Vorlage Pokemon
-/*
-const all_pokemon = await loadPokemon();
-let all_pokemon_with_details = [];
-
-try {
-  const response = await fetch(url);
-  const answer = await response.json();
-  return answer.results;
-} catch (error) {
-  console.error(error);
-  return false;
-}
-
-// pokemon loader
-async function loadPokemon() {
-  const url = "https://pokeapi.co/api/v2/pokemon?limit=9&offset=0"; // mit korrekter API-URL ersetzen
-  try {
-    const response = await fetch(url);
-    const answer = await response.json();
-    return answer.results;
-  } catch (error) {
-    console.error(error);
-    return false;
-  }
-}
-async function loadPokemonDetails(url) {
-  try {
-    const response = await fetch(url);
-    const answer = await response.json();
-    // array all_pokemon_with_details mit den details aus den detaildaten befüllen
-    return {
-      name: answer.name,
-      image: answer.sprites.front_default,
-      hp: answer.stats[0].base_stat,
-      type: answer.types[0].type.name,
-    };
-  } catch (error) {
-    console.error(error);
-    return error;
-  }
-}
-for (const [key, pokemon] of all_pokemon.entries()) {
-  const details = await loadPokemonDetails(pokemon.url);
-  all_pokemon_with_details.push(details);
-}
-
-// unsere pokemon ins HTML/DOM einfüllen
-const cards_container = document.querySelector("#cards");
-all_pokemon_with_details.forEach((pokemon) => {
-  const card = `<div class="card">
-                <h2 class="pokemon_name">${pokemon.name}</h2>
-                <img class="pokemon_image" src="${pokemon.image}" alt="">
-                <p class="pokemon_hp">Lebenskraft: <span class="value_hp">${pokemon.hp}</span></p>
-            </div>`;
-  cards_container.innerHTML += card;
-});
-*/
